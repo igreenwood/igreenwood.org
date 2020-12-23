3d3f001d00ba79a66e86f0d12ad7a58c66385cc8
igreenwood <i.greenwood.dev@gmail.com>
2020-12-23 15:21:04 +0900

    Add base files and header

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,41 +1,54 @@
 import React, { ReactNode } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
+import styles from './layout.module.css'
 
 type Props = {
   children?: ReactNode
   title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
-
-export default Layout
+export default function Layout({ children, title = '' }: Props) {
+  return (
+    <div className= { styles.container }>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@200&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Karma:wght@300&display=swap" rel="stylesheet"/>
+      </Head>
+      <header>
+        <div className = { styles.toolbar }>
+          <h1 className ={ styles.toolbarTitle }><Link href="/"><a>{ title }</a></Link></h1>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">
+                  <a>Works</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/bio">
+                  <a>Bio</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/posts">
+                  <a>Posts</a>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      {children}
+      <footer>
+        <hr />
+        <span>I'm here to stay (Footer)</span>
+      </footer>
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,7 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+  <Layout title="igreenwood">
     <p>
       <Link href="/about">
         <a>About</a>
